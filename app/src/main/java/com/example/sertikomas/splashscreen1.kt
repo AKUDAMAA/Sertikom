266edbf350e9ac05a0e7bc086cfb6de4ddbd4eda
Hilmi Zaki <hilmizaki784@gmail.com>
2023-12-04 06:53:16 +0700

    sertikom

---
+++ app/src/main/java/com/example/sertikomas/splashscreen1.kt
@@ -0,0 +1,13 @@
+package com.example.sertikomas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splashscreen1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen1)
+    }
+
+
+}
